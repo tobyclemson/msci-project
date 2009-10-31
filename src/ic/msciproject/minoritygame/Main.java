d71fa1c73c41b37ed85e225d9a68e0909c6cce73
Toby Clemson <tobyclemson@gmail.com>
2009-10-31 20:20:48 +0000

    Adding NetBeans support files and a .gitignore file to ignore build products.

---
+++ src/ic/msciproject/minoritygame/Main.java
@@ -0,0 +1,16 @@
+package ic.msciproject.minoritygame;
+
+/**
+ *
+ * @author Toby Clemson
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
