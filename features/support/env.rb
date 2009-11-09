b1e373b9f7d65b396e928a7b11264cd333fe3286
Toby Clemson <tobyclemson@gmail.com>
2009-11-09 02:57:06 +0000

    Fixing loadpath error in env.rb.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,2 +1,13 @@
+$LOAD_PATH.unshift File.expand_path(
+  File.join(
+    File.dirname(__FILE__), '..', '..', 'dist'
+  )
+) unless
+  $LOAD_PATH.include?(File.join(File.dirname(__FILE__), '..', '..', 'dist')) ||
+  $LOAD_PATH.include?(File.expand_path(
+    File.join(File.dirname(__FILE__), '..', '..', 'dist')
+  )
+)
+
 require 'java'
 require 'MSci_Project_Code.jar'
