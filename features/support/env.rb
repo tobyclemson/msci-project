a43e42a212cc65c8f372a1ebc3328eedf9f0d458
Toby Clemson <tobyclemson@gmail.com>
2009-11-11 12:16:43 +0000

    Fixing loadpath in Cucumber env.rb.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,11 +1,11 @@
 $LOAD_PATH.unshift File.expand_path(
   File.join(
-    File.dirname(__FILE__), '..', 'lib'
+    File.dirname(__FILE__), '..', '..', 'lib'
   )
 ) unless
-  $LOAD_PATH.include?(File.join(File.dirname(__FILE__), '..', 'lib')) ||
+  $LOAD_PATH.include?(File.join(File.dirname(__FILE__), '..', '..', 'lib')) ||
   $LOAD_PATH.include?(File.expand_path(
-    File.join(File.dirname(__FILE__), '..', 'lib')
+    File.join(File.dirname(__FILE__), '..', '..', 'lib')
   )
 )
 
