1291e03703dafa48c68d8f6f4a32ed3e1899daf9
Toby Clemson <tobyclemson@gmail.com>
2009-11-12 01:15:51 +0000

    Removing pointless System.out.println call in Main.java.

--- src/ic/msciproject/Main.java
+++ src/ic/msciproject/Main.java
@@ -1,4 +1,4 @@
-package ic.msciproject.minoritygame;
+package ic.msciproject;
 
 /**
  *
@@ -10,7 +10,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 
 }
